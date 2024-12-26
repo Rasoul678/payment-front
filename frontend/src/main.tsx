d8eb519db7e391de1bb986a226da8390d741254a
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2024-12-26 09:53:37 +0330

    :package::wrench: config tailwind and shadcn :)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
-import "./index.css";
+import "./styles/globals.css";
 
 const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
