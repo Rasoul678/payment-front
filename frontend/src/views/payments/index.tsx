7c40513ae8296b90806db8d0e5e1fff5486bd93b
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2025-01-04 15:30:33 +0330

    :zap: improve performance :)

--- frontend/src/views/payments/index.tsx
+++ frontend/src/views/payments/index.tsx
@@ -21,9 +21,16 @@ import { NavLink } from "react-router";
 type IProps = {};
 type PaymentsQueryType = APIResType<PaymentsListRType>;
 
+/**
+ * Highlights a given string with the provided highlight term.
+ *
+ * @param text - The text to be highlighted.
+ * @param highlight - The term to highlight within the text.
+ * @returns A React element with the highlighted text.
+ */
 const getHighlightedText = (text: string, highlight: string) => {
-  // Split text on highlight term, include term itself into parts, ignore case
   const parts = text.split(new RegExp(`(${highlight})`, "gi"));
+
   return (
     <span>
       {parts.map((part, i) =>
@@ -42,12 +49,14 @@ const getHighlightedText = (text: string, highlight: string) => {
 const PaymentList: React.FC<IProps> = () => {
   const { query } = useTypedSelector((state) => state.payments);
 
-  const { data, isLoading, isFetching, refetch } = useQuery<PaymentsQueryType>({
-    queryKey: ["payments", ...Object.values(query)],
-    queryFn: () => APIService.fetchPayments<PaymentsListRType>(query),
-    refetchOnWindowFocus: false,
-    retry: false,
-  });
+  const { data, isLoading, isFetching, refetch, isPlaceholderData } =
+    useQuery<PaymentsQueryType>({
+      queryKey: ["payments", ...Object.values(query)],
+      queryFn: () => APIService.fetchPayments<PaymentsListRType>(query),
+      placeholderData: (prev) => prev,
+      refetchOnWindowFocus: false,
+      retry: false,
+    });
 
   const payments = data?.result?.entities;
   const err = data?.error;
@@ -62,7 +71,7 @@ const PaymentList: React.FC<IProps> = () => {
         <SearchBar />
       </div>
       <div className="flex flex-col justify-between min-h-[calc(100vh-11rem)]">
-        {(isLoading || isFetching) && <Spinner />}
+        {(isLoading || isFetching) && !payments && <Spinner />}
         {err && !isFetching && (
           <div className="flex-1 flex flex-col items-center justify-center gap-2 h-full">
             <h2>{err}</h2>
@@ -71,51 +80,49 @@ const PaymentList: React.FC<IProps> = () => {
             </Button>
           </div>
         )}
-        {!isFetching && (
-          <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 items-center content-start gap-4 mt-2 mb-[1rem]">
-              {payments?.map((pay) => {
-                return (
-                  <Card className="w-auto h-[10rem]" key={pay.id}>
-                    <NavLink to={`/payments/${pay.id}`}>
-                      <CardHeader className="p-3 text-justify border-0 border-b-2">
-                        <CardTitle>
-                          <div className="relative inline-flex">
-                            <BadgeIcon
-                              data-effect="text"
-                              className={`h-3 w-3 ${pay.status}`}
-                            />
-                            <span className="capitalize ml-2">
-                              {pay.status}
-                            </span>
-                          </div>
-                        </CardTitle>
-                        <CardTitle className="capitalize">
-                          Type: {pay.type}
-                        </CardTitle>
-                        <CardTitle>
-                          Amount: {formatCurrency(pay.value)}
-                        </CardTitle>
-                      </CardHeader>
-                      <CardContent className="p-3 text-left h-[2.5rem] truncate italic">
-                        {pay.description
-                          ? query.search
-                            ? getHighlightedText(pay.description, query.search)
-                            : pay.description
-                          : '"No description provided"'}
-                      </CardContent>
-                      <CardFooter className="p-1 justify-center text-sm opacity-70">
-                        <CalendarIcon className="mr-2 h-4 w-4" />
-                        {formatDate(pay.paid_at)}
-                      </CardFooter>
-                    </NavLink>
-                  </Card>
-                );
-              })}
-            </div>
-            {payments?.length === 0 && <h1>No such payments exist yet!</h1>}
-          </>
-        )}
+
+        <div
+          className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 items-center content-start gap-4 mt-2 mb-[1rem] ${
+            isFetching && isPlaceholderData ? "opacity-50" : "opacity-100"
+          }`}
+        >
+          {payments?.map((pay) => {
+            return (
+              <Card className="w-auto h-[10rem]" key={pay.id}>
+                <NavLink to={`/payments/${pay.id}`}>
+                  <CardHeader className="p-3 text-justify border-0 border-b-2">
+                    <CardTitle>
+                      <div className="relative inline-flex">
+                        <BadgeIcon
+                          data-effect="text"
+                          className={`h-3 w-3 ${pay.status}`}
+                        />
+                        <span className="capitalize ml-2">{pay.status}</span>
+                      </div>
+                    </CardTitle>
+                    <CardTitle className="capitalize">
+                      Type: {pay.type}
+                    </CardTitle>
+                    <CardTitle>Amount: {formatCurrency(pay.value)}</CardTitle>
+                  </CardHeader>
+                  <CardContent className="p-3 text-left h-[2.5rem] truncate italic">
+                    {pay.description
+                      ? query.search
+                        ? getHighlightedText(pay.description, query.search)
+                        : pay.description
+                      : '"No description provided"'}
+                  </CardContent>
+                  <CardFooter className="p-1 justify-center text-sm opacity-70">
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {formatDate(pay.paid_at)}
+                  </CardFooter>
+                </NavLink>
+              </Card>
+            );
+          })}
+        </div>
+        {payments?.length === 0 && <h1>No such payments exist yet!</h1>}
+
         <DynamicPagination totalCount={totalCount} />
       </div>
     </section>
